b86ca4132291a8e08423782ed2a4da5ed0bf04f2
Fuad-Hellscream <49075537+Fuad-Hellscream@users.noreply.github.com>
2020-09-12 19:54:07 +0600

    3. Added Login and Registration ability from the front-end
    Users can now register new users and log into those accounts right from the web UI.

--- StudentSocial.API/Controllers/ValuesController.cs
+++ StudentSocial.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace StudentSocial.API.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
